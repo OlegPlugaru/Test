457b501aaab28692bb37f096008983bbb80090a5
ironMan <oleg.plugaru.md@gmail.com>
2022-12-05 17:34:30 +0200

    add variables.py

---
+++ Giraffe Academy/variables.py
@@ -0,0 +1,13 @@
+
+character_name = "George"
+character_age = "35"
+is_Male = True
+
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old.")
+
+if is_Male == False :
+    character_name = "Mike"
+
+print("He really liked the name " + character_name + ",")
+print("but didn't like being " + character_age + ".")
