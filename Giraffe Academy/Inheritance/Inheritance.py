affb070739861fbd1de82bf519b647b40c6dddcb
ironMan <oleg.plugaru.md@gmail.com>
2022-12-09 20:47:08 +0200

    Added Inheritance directory with 2 class files and main file

---
+++ Giraffe Academy/Inheritance/Inheritance.py
@@ -0,0 +1,13 @@
+from Chef import Chef
+from ChineseChef import ChineseChef
+from ItalianChef import ItalianChef
+
+myChef = Chef()
+myChef.make_chicken()
+
+myChineseChef = ChineseChef()
+myChineseChef.make_special_dish()
+
+myItalianChef = ItalianChef()
+myItalianChef.make_pasta()
+myItalianChef.make_special_dish()
---
+++ Giraffe Academy/Inheritance/ItalianChef.py
@@ -0,0 +1,8 @@
+from Chef import Chef
+
+class ItalianChef(Chef):
+
+    def make_special_dish(self):
+        print("The chef make Pizza")
+    def make_pasta(self):
+        print("The chef makes pasta")
